f5dec6cc1adce9c068a4c6da4e6b6ce68e1a3575
ApriyantoWibowo <pri.yant0@ovi.com>
2016-11-22 17:36:02 +0700

    12141362 menambahkan array nama pemain,skor pemain, dan menambahkan dua perintah input dengan scanner, serta menambahkan random untuk mengacak angka. Ketiga fungsi tersebut : array,scanner dan random harus diimport agar program bisa berjalan

---
+++ src/com/mysite/app12141362/TebakAngka.java
@@ -0,0 +1,16 @@
+package com.mysite.app12141362;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+/*
+ * @author Apriyanto
+ */
+public class TebakAngka {
+    public static void main(String[] args) {
+        ArrayList <String> namaPemain = new ArrayList<>();
+        ArrayList <Integer> skorPemain = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        Random angkaRandom = new Random();
+        
